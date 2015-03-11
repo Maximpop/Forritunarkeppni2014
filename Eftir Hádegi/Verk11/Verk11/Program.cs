8d9b3455a0e986550b10d4d3cd03a994a37d565f
Ómar Högni Guðmarsson <omsigum6@gmail.com>
2015-03-11 20:39:57 +0000

    bætti við nokkrum

---
+++ Eftir Hádegi/Verk11/Verk11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verk11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>[] buckets = new List<int>[10];
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                buckets[i] = new List<int>();
+            }
+            int[] nums = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                buckets[nums[i] / 10].Add(nums[i] % 10);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (buckets[i].Count == 0)
+                {
+                    continue;
+                }
+                buckets[i].Sort();
+                Console.Write(i + " ");
+                buckets[i].ForEach(x => Console.Write(x));
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Eftir Hádegi/Verk9/Verk9/Program.cs
+++ Eftir Hádegi/Verk9/Verk9/Program.cs
@@ -10,6 +10,26 @@ namespace Verk9
     {
         static void Main(string[] args)
         {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            int index = -1;
+            for (int i = s2.Length; i > 0 && index == -1; i--)
+            {
+                string s = s2.Substring(0, i);
+                if (s1.EndsWith(s))
+                {
+                    index = s1.LastIndexOf(s);
+                }
+            }
+            if (index == -1)
+            {
+                Console.WriteLine(s1 + s2);
+            }
+            else
+            {
+                Console.WriteLine(s1.Substring(0, index) + s2);
+            }
+            Console.ReadLine();
         }
     }
 }
