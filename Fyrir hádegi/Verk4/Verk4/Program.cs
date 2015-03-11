d271ca92c3b83c1e6a008ccc1b8eac6bb5a9fe54
Ómar Högni Guðmarsson <omsigum6@gmail.com>
2015-03-11 17:42:35 +0000

    bætti við dæmum

---
+++ Fyrir hádegi/Verk4/Verk4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verk4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Write(input + "! = ");
+            for (int i = 1; i < input; i++)
+            {
+                Console.Write(i + " x ");
+            }
+            Console.Write(input);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fyrir hádegi/Verk7/Verk7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verk7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string output = null;
+            for (int i = 0; i < input1.Length; i++)
+            {
+                output += input1[i].ToString() + input2[i].ToString();
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+    }
+}
